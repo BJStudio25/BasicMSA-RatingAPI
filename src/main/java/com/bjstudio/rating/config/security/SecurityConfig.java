40a5013565b4066578568dce99dcffea0ec9f444
BJStudio25 <bjstudio.it@gmail.com>
2020-04-13 11:48:39 +0200

    Added Eureka config.

--- src/main/java/com/bjstudio/rating/config/security/SecurityConfig.java
+++ src/main/java/com/bjstudio/rating/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * SecurityConfig
@@ -24,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
             .authorizeRequests()
                 .regexMatchers("^/ratings\\?bookId.*$").authenticated()
                 .antMatchers(HttpMethod.POST, "/ratings").authenticated()
---
+++ src/main/java/com/bjstudio/rating/config/session/SessionConfig.java
@@ -0,0 +1,15 @@
+package com.bjstudio.rating.config.session;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+/**
+ * SessionConfig
+ */
+@EnableRedisHttpSession
+@Configuration
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    
+}
